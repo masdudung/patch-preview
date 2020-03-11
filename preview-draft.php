889a33e3a4c6a7ddc315860ba08d99a3d096e38d
Moch Mufiddin <m.mufiddin@gmail.com>
2020-03-11 16:39:38 +0700

    Update preview-draft.php

--- preview-draft.php
+++ preview-draft.php
@@ -8,29 +8,30 @@
 
 class preview_draft{
     
-    function __construct()
-    {
-      #register rewrite rule
-      add_action('init',[$this, 'rewrite_rule']);
-      
-      #register filter preview_post_link
-      add_filter('init',[$this, 'filter_preview_post_link']);
-		}
+	function __construct()
+	{
+		#register rewrite rule
+		add_action('init',[$this, 'rewrite_rule']);
+
+		#register filter preview_post_link
+		add_filter('init',[$this, 'filter_preview_post_link']);
+	}
     
-    function filter_preview_post_link($link)
-    {
-      global $post;
-			if(isset($post->post_status) && $post->post_status == 'draft') {
-        $slug = basename(get_permalink());
-        return home_url("/preview/$slug");
-			}
-			return $link;
-    }
+	function filter_preview_post_link($link)
+	{
+	    global $post;
+	    if(isset($post->post_status) && $post->post_status == 'draft') {
+		    $slug = basename(get_permalink());
+		    return home_url("/preview/$slug");
+	    }
+	    return $link;
+	}
     
-    function rewrite_rule()
-    {
-      add_rewrite_rule('^preview?$', "index.php?preview=true");
-    }
+	function rewrite_rule()
+	{
+	    add_rewrite_rule('^preview?$', "index.php?preview=true");
+	}
 
 }
+
 $plugin = new preview_draft();
