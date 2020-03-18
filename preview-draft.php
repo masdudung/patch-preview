ef2ed7ae1174b9addf5767d6bb8b28e8bc592f56
Moch Mufiddin <m.mufiddin@gmail.com>
2020-03-18 13:58:08 +0700

    Update preview-draft.php

--- preview-draft.php
+++ preview-draft.php
@@ -1,37 +1,81 @@
 <?php
 /**
- * Plugin Name: preview draft
- * Version: 0.0.1
- * Author: SoftwareSeni Team
- * 
+ * Class preview_draft
+ *
+ * @author SoftwareSeni Team
  */
 
-class preview_draft{
-    
-	function __construct()
-	{
-		#register rewrite rule
-		add_action('init',[$this, 'rewrite_rule']);
-
-		#register filter preview_post_link
-		add_filter('preview_post_link',[$this, 'filter_preview_post_link']);
-	}
-    
-	function filter_preview_post_link($link)
-	{
-	    global $post;
-	    if(isset($post->post_status) && $post->post_status == 'draft') {
-		    $slug = basename(get_permalink());
-		    return home_url("/preview/$slug");
-	    }
-	    return $link;
-	}
-    
-	function rewrite_rule()
-	{
-	    add_rewrite_rule('^preview?$', "index.php?preview=true");
-	}
-
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
 }
 
-$plugin = new preview_draft();
+if ( ! class_exists( 'Ast_Preview_Draft' ) ) {
+
+	/**
+	 * Class Ast_Preview_Draft
+	 */
+	class Ast_Preview_Draft {
+
+		/**
+		 * Instance variable.
+		 *
+		 * @var null
+		 */
+		private static $instance = null;
+
+		/**
+		 * Singleton
+		 *
+		 * @return Ast_Preview_Draft|null
+		 */
+		public static function init() {
+			if ( null === self::$instance ) {
+				self::$instance = new self();
+			}
+
+			return self::$instance;
+		}
+
+		/**
+		 * Ast_Preview_Draft constructor.
+		 */
+		private function __construct() {
+
+			// Register rewrite rule.
+			add_action( 'init', [ $this, 'rewrite_rule' ] );
+
+			// Register filter preview_post_link.
+			add_filter( 'preview_post_link', [ $this, 'filter_preview_post_link' ], 10, 2 );
+		}
+
+		/**
+		 * Callback for previewing draft post link.
+		 *
+		 * @param string $link current preview link.
+		 * @param WP_Post $post object of the current post.
+		 *
+		 * @return string|void
+		 */
+		function filter_preview_post_link( $link, $post ) {
+			if isset($post->post_status){
+					
+				if ( 'draft' === $post->post_status ) {
+					$slug = basename( get_permalink( $post ) );
+
+					$link = home_url( "/blog/preview/$slug" );
+				}
+			}
+
+			return $link;
+		}
+
+		/**
+		 * Callback for adding rewrite rule.
+		 */
+		function rewrite_rule() {
+			add_rewrite_rule( '^blog/preview?$', "index.php?preview=true", 'top' );
+		}
+	}
+
+	Ast_Preview_Draft::init();
+}
